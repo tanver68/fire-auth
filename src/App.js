0d87e8388ab09a25fc60385c79917578835e9ef4
tanver68 <16101068@uap-bd.edu>
2021-01-15 01:17:49 +0600

    firebase use kore kivabe authorized information ane nijer website e dakhano jay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  
+  const [user,setUser] = useState({
+    // akhana atodin amra initially 1ta object 0 othoba empty array declear korci karon amader multy value dorkar cilo na bt aj dorkar tay object declear kore nilam 
+    isSignedIn: false,  //here initially jodi sign in false hoy tay declear korlam and other valu gulo faka that means 0 declear korlam
+    name: '',
+    email: '',
+    photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName,email,photoURL} = res.user;
+      const signInUser = {
+        // tar mane akhane amra upore j state gulo set kortaci sey sob jinish gulo niya nilam akhane
+        isSignedIn : true,
+        name : displayName,
+        email : email,
+        photo : photoURL
+      }
+      setUser(signInUser) // akhane user state e signInUser ta set hoyce
+      console.log(displayName,email,photoURL)
+
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.message);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handleSignIn}>Sign In</button>
+    {
+      user.isSignedIn && <p>Welcome {user.name}</p>  // akhane user.isSigndIn jodi true hoy tahole user ar name ta dakhabe
+    }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAn4KoXFdi8s6vJnFnXjS7Yr3vVUvLQ-1Y",
+    authDomain: "ema-john-simple-5d0ee.firebaseapp.com",
+    projectId: "ema-john-simple-5d0ee",
+    storageBucket: "ema-john-simple-5d0ee.appspot.com",
+    messagingSenderId: "324643296455",
+    appId: "1:324643296455:web:73a7dd3a11594260e9dae2"
+  };
+
+  export default firebaseConfig;
